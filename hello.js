48c0247817aa4ae9531bf24d61cea70024a63b6b
Ran Sel <ranselorm@outlook.com>
2022-05-23 21:03:05 +0000

    Add JS file

---
+++ hello.js
@@ -0,0 +1,12 @@
+
+
+let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"];
+
+let now = new Date();
+let dayName = daysOfTheWeek[now.getDay()];
+alert(`Hello Students. Happy ${dayName.toUpperCase()} to you all`)
+
+
+
+
